c96ba7a888263aed7e1a951a5248efbe19a68851
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-14 16:34:25 -0700

    Adjusted workout schema properties to include exercises

--- models/workout.js
+++ models/workout.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema ({
-    cardio: [
+    exercises: [
         {
         type: Schema.Types.ObjectId,
-        ref: "Cardio"
+        ref: "Exercise"
     }
 ]
 });
