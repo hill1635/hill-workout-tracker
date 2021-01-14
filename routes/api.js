2b9faadfee335441ee9e6f06e71e143fa82d2ffa
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-13 23:38:29 -0700

    API routes file started

---
+++ routes/api.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+// /api/workouts
+
+//GET request for last workout
+
+//PUT request to add exercise
+
+//POST request to create workout
+
+
+// /api/workouts/range
+
+//GET request for workouts in range
