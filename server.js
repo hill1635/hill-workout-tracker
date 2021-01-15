fe60e7331a7da05134039b3a679ad59b020ee85c
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-14 17:20:07 -0700

    get requests created for stats.html and index.html

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3000;
+const path = require("path");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+const public = path.join(__dirname, "/public");
+
 app.use(express.json());
-
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(require("./routes/api.js"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_app", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
 
-app.use(require("./routes/api.js"));
+app.get("/stats", function (req, res) {
+    res.sendFile(path.join(public, "stats.html"));
+});
+
+app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(public, "exercise.html"));
+});
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}.`);
