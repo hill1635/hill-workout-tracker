3b97376445dee02fbe24d8e3a6de17faf2a66e5f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-15 18:44:49 -0700

    GET route created to generate last seven workouts.

--- routes/api.js
+++ routes/api.js
@@ -46,4 +46,16 @@ router.put("/api/workouts/:id", function (req, res) {
     );
 });
 
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find({})
+        .sort({ day: -1 })
+        .limit(7)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
 module.exports = router;
