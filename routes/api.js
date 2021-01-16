10261c3abc8327979c7d448bc6f6cb927516b667
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-15 18:09:33 -0700

    Moved API routes on server.js to routes/api.js and fixed syntax and models.

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,49 @@
 const router = require("express").Router();
-const { Workout, Exercise } = require("../models");
+const Workout = require("../models/workout");
 
-// /api/workouts
+router.get("/api/workouts", function (req, res) {
+    Workout.find({})
+        .sort({ day: -1 })
+        .limit(1)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
-//GET request for last workout
+router.post("/api/workouts", function ({ body }, res) {
+    Workout.create(body)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
 
-//PUT request to add exercise
+});
 
-//POST request to create workout
-
-
-// /api/workouts/range
-
-//GET request for workouts in range
+router.put("/api/workouts/:id", function (req, res) {
+    Workout.findOneAndUpdate(
+        {
+            _id: req.params.id
+        },
+        {
+            $push: {
+                exercises: req.body
+            }
+        },
+        (error, edited) => {
+            if (error) {
+                console.log(error);
+                res.send(error);
+            } else {
+                console.log(edited);
+                res.send(edited);
+            }
+        }
+    );
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const router = require("./routes/api.js");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
 const public = path.join(__dirname, "/public");
 
 app.use(express.json());
@@ -28,53 +26,6 @@ app.get("/exercise", function (req, res) {
     res.sendFile(path.join(public, "exercise.html"));
 });
 
-app.get("/api/workouts", function (req, res) {
-    db.Workout.find({})
-        .sort({ day: -1 })
-        .limit(1)
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-});
-
-//Add workout
-app.post("/api/workouts", function ({ body }, res) {
-    db.Workout.create(body)
-        .then((dbWorkout) => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-
-});
-
-//Add exercise
-app.put("/api/workouts/:id", function (req, res) {
-    db.Workout.findOneAndUpdate(
-        {
-            _id: mongoose.Types.ObjectId(req.params.id)
-        },
-        {
-            $push: {
-                exercises: req.body
-            }
-        },
-        (error, edited) => {
-            if (error) {
-                console.log(error);
-                res.send(error);
-            } else {
-                // console.log("req.body: ", req.body.toString());
-                console.log(edited);
-                res.send(edited);
-            }
-        }
-    );
-});
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}.`);
 });
