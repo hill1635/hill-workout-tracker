971ab3e712b97e35d7564d3803699a776eea4d4e
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-15 18:28:35 -0700

    Mongoose connection updated to deploy to heroku

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(router);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 });
 
